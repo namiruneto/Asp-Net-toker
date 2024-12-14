6101ff391b6bca313436595b35b2462299098ada
namiruneto <cesfernando1212@gmail.com>
2024-12-13 20:41:33 -0500

    test(Infrastructure): se agrega pruebas unitarias de infrasestructure

---
+++ BackEnd.Test.xUnit/Infrastructure/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using BackEnd.Infrastructure.Repositories;
+using BackEnd.Entities.Requests.User;
+using Xunit;
+using FluentAssertions;
+
+namespace BackEnd.Infrastructure.Tests
+{
+    public class UserRepositoryTests
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserRepositoryTests()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        [Fact]
+        public void SaveUser_ShouldReturnTrue_WhenUserIsSaved()
+        {
+            // Arrange
+            var registerRequest = new RegisterRequestDto { Name = "John", Phone = "123456789" };
+
+            // Act
+            var result = _userRepository.SaveUser(registerRequest);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotificationUser_ShouldReturnTrue_WhenUserIsNotified()
+        {
+            // Arrange
+            var registerRequest = new RegisterRequestDto { Name = "John", Phone = "123456789" };
+
+            // Act
+            var result = _userRepository.NotificationUser(registerRequest);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
